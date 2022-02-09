6ccc1f9bf636ad55ed0a300ba698df7615b7c99c
Madison Law <madisonlaw1@gmail.com>
2022-02-09 15:28:34 -0700

    Add hydrationData property and filter method when new User is instantiated

--- src/UserRepository.js
+++ src/UserRepository.js
@@ -1,29 +1,34 @@
 // import userData from './data/users'
 import User from './User'
 
+//*
 class UserRepository {
   constructor(data){
     this.data = data;
+    this.userData = data.userData;
+    this.hydrationData = data.hydrationData;
     this.currentUser = {}
   }
 
+//*
   createNewUser(id) {
-    const user = this.data.find(user => user.id === id)
-    const newUser = new User(user)
+    const userData = this.userData.find(user => user.id === id)
+    const hydrationData = this.hydrationData.filter(entry => entry.userID === id)
+    const newUser = new User(userData, hydrationData)
     this.currentUser = newUser
     return newUser
   }
 
   findFriendsById(id) {
-    const friend = this.data.find(friend => friend.id === id)
+    const friend = this.userData.find(friend => friend.id === id)
     return friend
   }
 
   calculateAverageStepGoal() {
-    const totalSteps = this.data.reduce((total, num) => {
+    const totalSteps = this.userData.reduce((total, num) => {
       return total += num.dailyStepGoal
     }, 0)
-    return totalSteps / this.data.length
+    return totalSteps / this.userData.length
   }
 
   createUserFriendList() {
@@ -36,3 +41,34 @@ class UserRepository {
 }
 
 export default UserRepository;
+
+
+
+
+
+
+
+
+
+// Scripts instantiates a new UserRepository within the fetch function
+/* 
+
+👥 UserRepository ->
+    - holds all of the data (userData, hydrationData, sleepData)
+    - can create new User instance, find & return friends, calculate
+    averages across the data
+    - responsible for 
+    
+  👤 User ->
+    - holds an individual user's information
+    - can create a new Hyrdration profile instance
+    - responsible for ...
+
+     💧 HydrationProfile ->
+        - holds one user's hydration profile 
+        - responsible for:
+          * calculating how much water user consumes on any given day
+          * how much water user has consumed over 1 week
+          * average daily amount of water user has ever consumed
+
+*/
