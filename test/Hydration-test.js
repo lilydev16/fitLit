0aba8b4cd07ab4be2980401a63c36b595a70f1b5
Madison Law <madisonlaw1@gmail.com>
2022-02-09 16:56:37 -0700

    Add tests for calcOuncesPerDay method

--- test/Hydration-test.js
+++ test/Hydration-test.js
@@ -1,30 +1,53 @@
-// import { expect } from 'chai';
-// import Hydration from '../src/Hydration';
+import { expect } from 'chai';
+import Hydration from '../src/Hydration';
 
-// let userHydration, hydrationData
+let userHydration, hydrationData
 
-// describe('User Repository', () => {
+describe('HydrationProfile', () => {
 
-//   beforeEach(function() {
-//     hydrationData = 
-//     {
-//       "userID": 1,
-//       "date": "2019/06/16",
-//       "numOunces": 69
-//   };
+  beforeEach(function() {
+    hydrationData = [
+    {
+      "userID": 1,
+      "date": "2019/06/16",
+      "numOunces": 69
+  },
+  {
+    "userID": 2,
+    "date": "2019/06/15",
+    "numOunces": 75
+  }, 
+  {
+    "userID": 3,
+    "date": "2019/06/15",
+    "numOunces": 47
+},
+{
+    "userID": 4,
+    "date": "2019/06/15",
+    "numOunces": 85
+}];
 
-//   userHydration = new Hydration(hydrationData)
+  userHydration = new Hydration(hydrationData)
 
-//   })
+  })
 
-//   it('should be a function', function () {
-//     expect(Hydration).to.be.a('function');
-//   });
+  it('should be a function', function () {
+    expect(Hydration).to.be.a('function');
+  });
 
-//   it('should instantiate a User', function () {
-//     expect(userHydration).to.be.an.instanceof(Hydration)
-//   });
+  it('should instantiate a HydrationProfile', function () {
+    expect(userHydration).to.be.an.instanceof(Hydration)
+  });
+
+  it('should be able to hold a user\'s hydration data', function () {
+    expect(userHydration.hydrationData).to.equal(hydrationData)
+  })
+
+  it('should be able to calculate ounces per day based on date', function() {
+    expect(userHydration.calcOuncesPerDay("2019/06/16")).to.equal(69)
+  } )
 
 
 
-// })
+})
