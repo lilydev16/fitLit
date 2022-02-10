f70d667f9accb0c4e42f5ffe3f28f1240f6f67bd
lswatson16 <lauralynw16@gmail.com>
2022-02-10 17:24:05 -0500

    Add fetch to the sleep data

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -8,11 +8,16 @@ function fetchData() {
     .then(response => response.json())
     .then(data => data.hydrationData)
 
-  return Promise.all([allUserData, allHydrationData])
+  const allSleepData = fetch('https://fitlit-api.herokuapp.com/api/v1/sleep')
+    .then(response => response.json())
+    .then(data => data.sleepData)
+
+  return Promise.all([allUserData, allHydrationData, allSleepData])
     .then(data => {
       let allData = {};
       allData.userData = data[0];
-      allData.hydrationData = data[1]
+      allData.hydrationData = data[1];
+      allData.sleepData = data[2]
       return allData;
     })
 
