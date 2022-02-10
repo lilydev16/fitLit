43f97f28f418f236440ad2f1d65dc01523e7a5be
lswatson16 <lauralynw16@gmail.com>
2022-02-10 18:17:17 -0500

    Revise test to make webpack happy

--- test/Sleep-test.js
+++ test/Sleep-test.js
@@ -11,19 +11,14 @@ describe('Sleep', () => {
       {userID: 2, date: '2019/06/15', hoursSlept: 7, sleepQuality: 4.7},
       {userID: 3, date: '2019/06/15', hoursSlept: 10.8, sleepQuality: 4.7}
     ]
-
   })
 
-  it('should be a function', function () {
+  it('should be a function', function() {
     expect(Sleep).to.be.a('function');
-  });
+  })
 
   it('should instantiate a Sleep', function () {
-
     expect(sleep).to.be.an.instanceof(Sleep)
   });
 
-
-
-
-}
+})
