e1b66a267eaf1a34dd670f651c8c289bc9ac6676
Madison Law <madisonlaw1@gmail.com>
2022-02-12 18:45:39 -0700

    Add in function to handle errors with API

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,16 +1,21 @@
+import handleApiErrors from "./scripts"
+
 function fetchData() {
 
   const allUserData = fetch('https://fitlit-api.herokuapp.com/api/v1/users')
     .then(response => response.json())
     .then(data => data.userData)
+    .catch(err => handleApiErrors())
 
   const allHydrationData = fetch('https://fitlit-api.herokuapp.com/api/v1/hydration')
     .then(response => response.json())
     .then(data => data.hydrationData)
+    .catch(err => handleApiErrors())
 
   const allSleepData = fetch('https://fitlit-api.herokuapp.com/api/v1/sleep')
     .then(response => response.json())
     .then(data => data.sleepData)
+    .catch(err => handleApiErrors())
 
   return Promise.all([allUserData, allHydrationData, allSleepData])
     .then(data => {
