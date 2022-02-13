c4bfc824f67fc6196afefbdf8c9f0c0fafee03f5
Nicole Valentini <ncvalentini@gmail.com>
2022-02-13 16:09:51 -0700

    Update friends

--- src/scripts.js
+++ src/scripts.js
@@ -76,7 +76,6 @@ function updateUserProfile(user, data) {
   userStride.innerText = ` Stride Length: ${user.strideLength}`
   userStepGoal.innerText = `Step Goal: ${user.dailyStepGoal}`
   avgStepGoal.innerText = `Compare your step goal to the FitLit community: ${data.calcAvgStatsForAllUsers('dailyStepGoal', 'userData')}`
-  // friendList.innerText = `${data.createUserFriendList()}`
   updateFriends(data)
 }
 
