4abb8b45fe86b4c8be5ab5c5aea2097f48ea83c3
Nicole Valentini <ncvalentini@gmail.com>
2022-02-22 17:58:14 -0700

    Change fetch url

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,7 +1,7 @@
 import handleApiErrors from "./scripts";
 
-function fetchData(url, endData) {
-  return fetch(`https://fitlit-api.herokuapp.com/api/v1/${url}`)
+function fetchData(url) {
+  return fetch(`http://localhost:3001/api/v1/${url}`)
     .then(response => response.json())
     .catch(err => handleApiErrors())
 };
